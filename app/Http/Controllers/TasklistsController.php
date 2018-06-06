2dc798558a06083bdd0a3b9ef94a8da061679943
anna.hirai <tomato@gmail.com>
2018-06-06 07:04:01 +0000

    validation

--- app/Http/Controllers/TasklistsController.php
+++ app/Http/Controllers/TasklistsController.php
@@ -44,6 +44,10 @@ class TasklistsController extends Controller
      */
     public function store(Request $request)
     {
+        $this->validate($request, [
+            'content' => 'required|max10',
+            ]);
+        
         $tasklist = new Tasklist;
         $tasklist->content = $request->content;
         $tasklist->save();
@@ -91,6 +95,10 @@ class TasklistsController extends Controller
      */
     public function update(Request $request, $id)
     {
+        $this->validate($request, [
+            'content' => 'required|max:10',
+            ]);
+            
         $tasklist = Tasklist::find($id);
         $tasklist->content = $request->content;
         $tasklist->save();
